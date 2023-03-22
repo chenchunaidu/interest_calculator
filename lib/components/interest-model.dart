313dc70191e428d25f42e7d429eb7d630b8f9097
chenchunaidu-hotstar <chenchunaidu.m.con@hotstar.com>
2023-03-22 23:27:07 +0530

    fix: update formatted date

--- lib/components/interest-model.dart
+++ lib/components/interest-model.dart
@@ -31,6 +31,53 @@ class InterestModel {
     return "${getDiffInDays()} days";
   }
 
+  String formattedDiff() {
+    var startDate = DateTime.parse(this.startDate);
+    var endDate = DateTime.parse(this.endDate);
+    var monthAddValue = 0;
+    var yearAddValue = 0;
+    var diffDate = endDate.day - startDate.day;
+
+    if (endDate.day < startDate.day) {
+      DateTime now = DateTime.now();
+      int lastday = DateTime(now.year, now.month + 1, 0).day;
+      diffDate = lastday - startDate.day + endDate.day;
+      monthAddValue = -1;
+    }
+    var diffMonth = endDate.month - startDate.month;
+    if (endDate.month < startDate.month) {
+      diffMonth = 12 + endDate.month - startDate.month;
+      yearAddValue = -1;
+    }
+    var diffYear = endDate.year - startDate.year;
+    var diffString = "";
+    if (diffYear + yearAddValue > 0) {
+      diffString += "${diffYear + yearAddValue} Year";
+      if (diffYear + yearAddValue > 1) {
+        diffString += "s ";
+      } else {
+        diffString += " ";
+      }
+    }
+    if (monthAddValue + diffMonth > 0) {
+      diffString += "${diffMonth + monthAddValue} Month";
+      if (monthAddValue + diffMonth > 1) {
+        diffString += "s ";
+      } else {
+        diffString += " ";
+      }
+    }
+    if (diffDate > 0) {
+      diffString += "$diffDate day";
+      if (diffDate > 1) {
+        diffString += "s ";
+      } else {
+        diffString += " ";
+      }
+    }
+    return diffString;
+  }
+
   String getSimpleTotalValue() {
     return ((principleAmount *
             (1 + ((interestRate * getDiffInDays()) / 36500))))
@@ -45,8 +92,9 @@ class InterestModel {
   }
 
   InterestPreviewModel getSimpleInterestPreview() {
+    formattedDiff();
     return InterestPreviewModel(
-        timePeriod: getTimePeriod(),
+        timePeriod: formattedDiff(),
         totalAmount: getSimpleTotalValue(),
         principleAmount: principleAmount.toString(),
         interestRate: interestRate.toString());
@@ -54,7 +102,7 @@ class InterestModel {
 
   InterestPreviewModel getCompoundInterestPreview() {
     return InterestPreviewModel(
-        timePeriod: getTimePeriod(),
+        timePeriod: formattedDiff(),
         totalAmount: getCompoundTotalValue(),
         principleAmount: principleAmount.toString(),
         interestRate: interestRate.toString());
